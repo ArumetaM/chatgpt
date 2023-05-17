60ff45e717ef4846190433d763e9cfbafc78a2c7
ArumetaM <y-suematsu@freee.co.jp>
2023-05-17 16:43:09 +0900

    tokens数削減のためにやりとりするturn数の設定、初期に設定したassistantを直前に設定することで保存する機能

--- dialogue.py
+++ dialogue.py
@@ -2,8 +2,29 @@ import openai
 import pickle
 import os
 
+def cut_message(message,turns):
+    return message[max(0,len(message)-turns*2+1):]
+
+def add_assistant_message_latest(message):
+    if message[0]["role"] == "assistant":
+        message_assistant = message.pop(0)
+        message.insert(len(message)-1,message_assistant)
+    return message
+
+def preprocess(message,turns,support):
+    if support:
+        message = add_assistant_message_latest(message)
+    message = cut_message(message,turns)
+    return message
+
 print("タイトルを入力してください")
 title = input()
+print("保存する対話ターン数を指定してください。指定しない場合は5です。")
+turns = input()
+if turns == "":
+    turns = 5
+else:
+    turns = int(turns)
 if os.path.isfile("history/pic/{}.pic".format(title)):
     with open("history/pic/{}.pic".format(title), 'rb') as f:
         dialogue_message = pickle.load(f)
@@ -16,6 +37,12 @@ else:
     system_setting = input()
     if system_setting != "":
         dialogue_message.append({"role": "system","content": system_setting})
+    print("エージェントへの支持を直前に持っていきますか？(yes/no)、指定しない場合はnoです。")
+    support = input()
+    if support == "yes":
+        support = True
+    else:
+        support = False
 
 flag=True
 
@@ -37,10 +64,11 @@ while flag:
         exit()
     else:
         dialogue_message.append({"role": "user","content": text})
-    
+
+    send_text_data = preprocess(dialogue_message,turns,support)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=dialogue_message
+        messages=send_text_data
         )
     print(response["choices"][0]["message"]["content"])
     dialogue_message.append({"role": "assistant","content": response["choices"][0]["message"]["content"]})
@@ -53,4 +81,3 @@ while flag:
         text_data += "role:{}\ncontext:{}\n".format(line["role"],line["content"])
     with open("history/text/{}.txt".format(title),"w",encoding="utf-8") as f:
         f.write(text_data)
-
